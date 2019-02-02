34fda6e65354836854fce8ee9d86b7888c02c232
Vlad <vlad.velichko.90@gmail.com>
2019-02-02 17:44:15 +0200

    I JUST DO IT
    i create fucking triangle wich can rotate

--- SignInPage.js
+++ SignInPage.js
@@ -1,1 +1,11 @@
-
+var angle = 180;
+function rot() {
+  var time = setInterval (function(){
+    if (angle > 360){cleanInterval(time);}
+    else {
+  var t = document.getElementById("trianglee");
+  t.style.transform = "rotate("+angle+"deg)";
+  angle+=1;
+}
+},5);
+}
